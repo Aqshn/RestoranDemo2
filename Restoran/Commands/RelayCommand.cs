d751a114e32d9a9450a393db94942eef068a6c95
user <user@DESKTOP-BE8CPQG>
2023-04-05 09:13:12 -0700

    Add project files.

---
+++ Restoran/Commands/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using Restoran.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Restoran.Commands
+{
+    public class RelayCommand : ICommand
+    {
+        private Action<object?> executeShowCommand;
+        private Func<object?, bool> canExecuteShowCommand;
+
+        public RelayCommand(Action<object?> executeShowCommand, Func<object?, bool> canExecuteShowCommand)
+        {
+            this.executeShowCommand = executeShowCommand;
+            this.canExecuteShowCommand = canExecuteShowCommand;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public void Execute(object? parameter)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+        }
+
+        public bool CanExecute(object? parameter) => true;
+
+
+    }
+}
---
+++ Restoran/Views/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Restoran.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Restoran.Views
+{
+    /// <summary>
+    /// Interaction logic for View.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public ICommand ShowCommand { get; set; }
+
+
+        public void Click(object sender, RoutedEventArgs e)
+        {
+            ShowCommand = new RelayCommand(ExecuteShowCommand1, CanExecuteShowCommand1);
+        }
+
+        void ExecuteShowCommand1(object? parametr)
+        {
+            
+        }
+        bool CanExecuteShowCommand1(object? parametr)
+            => btn1.Background == new SolidColorBrush(Colors.Green);
+    }
+
+}
